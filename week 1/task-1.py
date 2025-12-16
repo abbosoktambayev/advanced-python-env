df21056c783ebdddcf5fd550c90ecff95a61531b
abbosoktambayev <oktambayevabbos1@gmail.com>
2025-12-16 19:30:39 +0500

    first

---
+++ week 1/task-1.py
@@ -0,0 +1,13 @@
+# Get user input
+last_name = input("Your last name? ")
+first_name = input("Your first name? ")
+age = input("How old are you? ")
+phone_number = input("Your phone number? ")
+
+# Output the information
+print(f"Your {first_name}, {last_name}")
+print(f"Your {age}")
+print(f"Your {phone_number}")
+
+
+print("Hello World!")
