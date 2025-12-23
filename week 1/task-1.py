edf4a92d09a7b8e516c1496b5018e8dfcf38357e
abbosoktambayev <oktambayevabbos1@gmail.com>
2025-12-23 20:14:03 +0500

    task 1

--- week 1/task-1.py
+++ week 1/task-1.py
@@ -1,13 +1,25 @@
-# Get user input
-last_name = input("Your last name? ")
-first_name = input("Your first name? ")
-age = input("How old are you? ")
-phone_number = input("Your phone number? ")
+# last_name = input("Your last name? ")
+# first_name = input("Your first name? ")
+# age = input("How old are you? ")
+# phone_number = input("Your phone number? ")
+#
+# print(f"Your {first_name}, {last_name}")
+# print(f"Your {age}")
+# print(f"Your {phone_number}")
+#
+#
+# print("Hello World!")
 
-# Output the information
-print(f"Your {first_name}, {last_name}")
-print(f"Your {age}")
-print(f"Your {phone_number}")
 
+s = input("Enter a sequence of arrows (>, <, -): ")
 
-print("Hello World!")
+cnt = 0
+target1 = ">>-->"
+target2 = "<--<<"
+
+for i in range(len(s) - 4):
+    sub = s[i : i+5]
+    if sub == target1 or sub == target2:
+        cnt += 1
+
+print("Count of arrows:", cnt)
